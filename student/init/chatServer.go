295bb6b24cbdbdf4b2a34114909d39f5ef13ef8c
chen406226 <chen406226@163.com>
2020-08-17 20:18:11 +0800

    m

--- student/init/chatServer.go
+++ student/init/chatServer.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"student/global"
 	chatService "student/service/chat"
+	"time"
 )
 
 //!+broadcaster
@@ -124,9 +125,12 @@ func handleConn(conn net.Conn) {
 	var fromUser string
 	for input.Scan() {
 		if linked {
-			messages <- who + ": " + input.Text()
+			//messages <- who + ": " + input.Text()
+			t := time.Now()
+			tS := t.Format("2006-01-02 15:04:05")[5:]
+			messages <- tS + ": " + input.Text()
 		} else {
-			flysnowRegexp := regexp.MustCompile(`From\:(.{3})To\:(.{3})Room\:\[([\d]{4})\](.*)`)
+			flysnowRegexp := regexp.MustCompile(`From\:(.{36})To\:(.{36})Room\:\[([\d]{4})\](.*)`)
 			params := flysnowRegexp.FindStringSubmatch(input.Text())
 			if len(params) <1 {
 				continue
