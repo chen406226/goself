a7ee2fbf4bca38df4ba5443f98fc2e6b873cf236
chen406226 <chen406226@163.com>
2020-11-30 22:58:30 +0800

    m

--- student/service/sys_dictionary.go
+++ student/service/sys_dictionary.go
@@ -5,6 +5,7 @@ import (
 	"gorm.io/gorm"
 	"student/global"
 	"student/model/mysqlDb"
+	"student/model/request"
 )
 
 // @title    CreateSysDictionary
@@ -21,13 +22,84 @@ func CreateSysDictionary(d mysqlDb.SysDictionary) (err error) {
 	}
 }
 
+// @title    DeleteSysDictionary
+// @description   delete a SysDictionary
+// @auth                     （2020/04/05  20:22）
+// @param     sysDictionary               model.SysDictionary
+// @return                    error
+
+func DeleteSysDictionary(d mysqlDb.SysDictionary) (err error) {
+	err = global.GL_DB.Delete(d).Delete(&d.SysDictionaryDetails).Error
+	return err
+}
 
 
+// @title    UpdateSysDictionary
+// @description   update a SysDictionary
+// @param     sysDictionary          *model.SysDictionary
+// @auth                     （2020/04/05  20:22）
+// @return                    error
 
+func UpdateSysDictionary(d *mysqlDb.SysDictionary) (err error) {
+	var dict mysqlDb.SysDictionary
+	sysDictionaryMap := map[string]interface{}{
+		"Name":		d.Name,
+		"Type": 	d.Type,
+		"Status":	d.Status,
+		"Desc": 	d.Desc,
+	}
+	db := global.GL_DB.Where("id = ?",d.ID).First(&dict)
+	if dict.Type == d.Type {
+		err = db.Updates(sysDictionaryMap).Error
+	} else {
+		if (!errors.Is(global.GL_DB.First(&mysqlDb.SysDictionary{},"type = ?",d.Type).Error,gorm.ErrRecordNotFound)) {
+			return errors.New("存在相同的type,不允许创建")
+		}
+		err = db.Updates(sysDictionaryMap).Error
+	}
+	return err
+}
 
+// @title    GetSysDictionary
+// @description   get the info of a SysDictionary
+// @auth                     （2020/04/05  20:22）
+// @param     id              uint
+// @return                    error
+// @return    SysDictionary        SysDictionary
 
+func GetSysDictoinary(Type string,Id uint) (err error, sysDictionary mysqlDb.SysDictionary) {
+	err = global.GL_DB.Where("type = ? or id = ?",Type,Id).Preload("SysDictionaryDetails").
+		First(&sysDictionary).Error
+	return
+}
 
+// @title    GetSysDictionaryInfoList
+// @description   get SysDictionary list by pagination, 分页获取用户列表
+// @auth                     （2020/04/05  20:22）
+// @param     info            PageInfo
+// @return                    error
 
+func GetSysDictionaryInfoList(info request.SysDictionarySearch) (err error,list interface{},total int64) {
+	limit := info.PageSize
+	offset := limit*(info.Page - 1)
+	db := global.GL_DB.Model(&mysqlDb.SysDictionary{})
+	var sysDictoinarys [] mysqlDb.SysDictionary
+	if info.Name != "" {
+		db = db.Where("`name` like ?","%"+info.Name+"%")
+	}
+	if info.Type != "" {
+		db = db.Where("`type` like ?","%"+info.Type+"%")
+	}
+	if info.Status != nil {
+		db = db.Where("`status` = ?",info.Status)
+	}
+	if info.Desc != "" {
+		db = db.Where("`desc` like ?","%"+info.Desc+"%")
+	}
+	err = db.Count(&total).Error
+	err = db.Limit(limit).Offset(offset).Find(&sysDictoinarys).Error
+	return err, sysDictoinarys, total
+}
 
 
 
--- student/service/sys_dictionary_detail.go
+++ student/service/sys_dictionary_detail.go
@@ -1,1 +1,80 @@
 package service
+
+import (
+	"student/global"
+	"student/model/mysqlDb"
+	"student/model/request"
+)
+
+// @title    CreateSysDictionaryDetail
+// @description   create a SysDictionaryDetail
+// @param     sysDictionaryDetail               model.SysDictionaryDetail
+// @auth                     （2020/04/05  20:22）
+// @return    err             error
+
+func CreateSystionaryDetail(sysDictionaryDetail mysqlDb.SysDictionaryDetail) (err error) {
+	err = global.GL_DB.Create(&sysDictionaryDetail).Error
+	return
+}
+
+// @title    DeleteSysDictionaryDetail
+// @description   delete a SysDictionaryDetail
+// @auth                     （2020/04/05  20:22）
+// @param     sysDictionaryDetail               model.SysDictionaryDetail
+// @return                    error
+
+func DeleteSysDictionaryDetail(sysDictioanryDetail mysqlDb.SysDictionaryDetail) (err error) {
+	err = global.GL_DB.Delete(&sysDictioanryDetail).Error
+	return
+}
+
+// @title    UpdateSysDictionaryDetail
+// @description   update a SysDictionaryDetail
+// @param     sysDictionaryDetail          *model.SysDictionaryDetail
+// @auth                     （2020/04/05  20:22）
+// @return                    error
+
+func UpdateSysDictionaryDetail(sysDictionaryDetail *mysqlDb.SysDictionaryDetail) (err error) {
+	err = global.GL_DB.Save(sysDictionaryDetail).Error
+	return
+}
+
+// @title    GetSysDictionaryDetail
+// @description   get the info of a SysDictionaryDetail
+// @auth                     （2020/04/05  20:22）
+// @param     id              uint
+// @return                    error
+// @return    SysDictionaryDetail        SysDictionaryDetail
+
+func GetSysDictionaryDetail(id uint) (err error, sysDictionaryDetail mysqlDb.SysDictionaryDetail) {
+	err = global.GL_DB.Where("id = ?",id).First(&sysDictionaryDetail).Error
+	return
+}
+
+// @title    GetSysDictionaryDetailInfoList
+// @description   get SysDictionaryDetail list by pagination, 分页获取用户列表
+// @auth                     （2020/04/05  20:22）
+// @param     info            PageInfo
+// @return                    error
+
+func GetSysDictionaryDetailInfoList(info request.SysDictionaryDetailSearch) (err error, list interface{},total int64) {
+	limit := info.PageSize
+	offset := limit*(info.Page -1)
+	db := global.GL_DB.Model(&mysqlDb.SysDictionaryDetail{})
+	var sysDictionaryDetails []mysqlDb.SysDictionaryDetail
+	if info.Label != "" {
+		db = db.Where("label like ?","%"+info.Label+"%")
+	}
+	if info.Value != 0 {
+		db = db.Where("value = ?",info.Value)
+	}
+	if info.Status != nil {
+		db = db.Where("status = ?",info.Status)
+	}
+	if info.SysDictionaryID != 0 {
+		db = db.Where("sys_dictionary_id = ?",info.SysDictionaryID)
+	}
+	err = db.Count(&total).Error
+	err = db.Limit(limit).Offset(offset).Find(&sysDictionaryDetails).Error
+	return err ,sysDictionaryDetails,total
+}
