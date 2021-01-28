631959526414cf764c88ef172f29f5b11890b775
chenkun <chenkun>
2021-01-28 17:58:30 +0800

    fyneUI

---
+++ gopl.io/ch1/server6/main.go
@@ -0,0 +1,89 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+
+// See page 21.
+
+// Server3 is an "echo" server that displays request parameters.
+package main
+
+import (
+    "encoding/json"
+    "fyne.io/fyne/v2/app"
+    "fyne.io/fyne/v2/container"
+    "fyne.io/fyne/v2/widget"
+    "io/ioutil"
+    "log"
+    "net/http"
+    "net/http/httputil"
+    "net/url"
+)
+
+var post = "8088"
+var iPost = "8777"
+var proxyHost = "http://localhost:8088"
+
+func main() {
+    a := app.New()
+    w := a.NewWindow("Hello")
+
+    hello := widget.NewLabel("Hello Fyne!")
+    w.SetContent(container.NewVBox(
+        hello,
+        widget.NewButton("Hi!", func() {
+            hello.SetText("Welcome :)")
+        }),
+    ))
+
+    w.ShowAndRun()
+    //fmt.Println(LoadConfig())
+    //init := Config{Name: "不知道你说什么东西"}
+    //SaveConfig(&init)
+}
+
+type Config struct {
+    Name   string  `json:"name"`
+}
+
+var configPath = "./config.json"
+
+func SaveConfig(config *Config){
+
+    data,err:=json.Marshal(config)
+    if err!=nil{
+        log.Fatal(err)
+    }
+    err=ioutil.WriteFile(configPath,data,0660)
+    if err!=nil{
+        log.Fatal(err)
+    }
+
+}
+
+func LoadConfig()(config *Config){
+    data,err:=ioutil.ReadFile(configPath)
+    if err!=nil{
+        log.Fatal(err)
+    }
+    //for _, line := range strings.Split(string(data), "\n") {
+    //    fmt.Println(line)
+    //}
+    config =&Config{}
+    err=json.Unmarshal(data,&config)
+    if err!=nil{
+        log.Fatal(err)
+    }
+    return config
+}
+
+//!+handler
+// handler echoes the HTTP request.
+func handler(w http.ResponseWriter, r *http.Request) {
+    remote, err := url.Parse(proxyHost)
+    if err != nil {
+        panic(err)
+    }
+    r.Host = "localhost:"+iPost
+    proxy := httputil.NewSingleHostReverseProxy(remote)
+    proxy.ServeHTTP(w, r)
+}
+
